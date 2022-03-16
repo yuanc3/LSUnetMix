7bcc3cdba25c8d82115e53e8f49ab5e13a5ea8fb
yuanc3 <yuanc0611@163.com>
2022-03-16 21:55:16 +0800

    update

--- nets/LSUnetMix.py
+++ nets/LSUnetMix.py
@@ -86,37 +86,6 @@ class DownBlock(nn.Module):
         out = self.maxpool(x)
         return self.nConvs(out)
 
-class Flatten(nn.Module):
-    def forward(self, x):
-        return x.view(x.size(0), -1)
-
-class CCA(nn.Module):
-    """
-    CCA Block
-    """
-    def __init__(self, F_g, F_x):
-        super().__init__()
-        self.mlp_x = nn.Sequential(
-            Flatten(),
-            nn.Linear(F_x, F_x))
-        self.mlp_g = nn.Sequential(
-            Flatten(),
-            nn.Linear(F_g, F_x))
-        self.relu = nn.ReLU(inplace=True)
-
-    def forward(self, g, x):
-        # channel-wise attention
-        avg_pool_x = F.avg_pool2d( x, (x.size(2), x.size(3)), stride=(x.size(2), x.size(3)))
-        channel_att_x = self.mlp_x(avg_pool_x)
-        avg_pool_g = F.avg_pool2d( g, (g.size(2), g.size(3)), stride=(g.size(2), g.size(3)))
-        channel_att_g = self.mlp_g(avg_pool_g)
-        channel_att_sum = (channel_att_x + channel_att_g)/2.0
-        scale = torch.sigmoid(channel_att_sum).unsqueeze(2).unsqueeze(3).expand_as(x)
-        x_after_channel = x * scale
-        out = self.relu(x_after_channel)
-        return out
-
-
 
 class MDC(nn.Module):
     def __init__(self, out_channels, activation='ReLU'):
